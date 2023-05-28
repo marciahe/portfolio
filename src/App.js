1abfc08ad598310deae2f2d5545fd15e07d45e9a
Marcia Hernández <ht.marcia@gmail.com>
2023-05-27 23:36:19 -0600

    Logo ready, Header WIP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import "./App.css";
-// import { Route, useLocation } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./views/Home/Home";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Boilerplate</h1>
-      <img
-        src="https://i.giphy.com/media/1NYkJ0wTvncdXV5dN5/giphy.webp"
-        alt="Happy coding"
-      />
-      {/* {location.pathname !== "/" && <NavBar />}
-      <Route exact path="/" component={Landing} />
+      {/* {location.pathname !== "/" && <Header />} */}
+
+      <Route path="*" component={Header} />
       <Route path="/home" component={Home} />
-      <Route path="/create" component={Form} />
-      <Route path="/detail/:id" component={Detail} /> */}
+      {/* <Route path="/create" component={Form} /> */}
+      {/* <Route path="/detail/:id" component={Detail} />  */}
     </div>
   );
 }
