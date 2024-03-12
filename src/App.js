3149d76400ace7efe297213200e3e81f68592c72
Marcia H <ht.marcia@gmail.com>
2024-03-11 23:45:38 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom'; // Asegúrate de importar Switch y Redirect
 import "./App.css";
-import { Route, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./views/Home/Home";
 
 function App() {
-  const location = useLocation();
   return (
     <div className="App">
-      {/* {location.pathname !== "/" && <Header />} */}
+      {/* Eliminamos el uso de useLocation y controlamos la visibilidad del Header con una Route */}
+      <Route path="/" component={Header} />
 
-      <Route path="*" component={Header} />
-      <Route path="/home" component={Home} />
-      {/* <Route path="/create" component={Form} /> */}
-      {/* <Route path="/detail/:id" component={Detail} />  */}
+      {/* Envuelve tus rutas en un Switch para que solo se renderice la primera que coincida */}
+      <Switch>
+        <Route path="/home" component={Home} />
+        {/* <Route path="/create" component={Form} />
+        <Route path="/detail/:id" component={Detail} /> */}
+        
+        {/* Redirección de / a /home */}
+        <Redirect from="/" to="/home" exact />
+      </Switch>
     </div>
   );
 }
